97e1022ed2bdc9ee9c6252f00e8855477c162988
Michael Spencer <ms53150@gmail.com>
2022-12-20 13:29:47 -0500

    add example for lists in react

--- Section_33-React-js/react-basics/src/index.js
+++ Section_33-React-js/react-basics/src/index.js
@@ -2,4 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello World!</h1>);
+root.render(
+	// <div>
+	// 	<h1>Hello World!</h1>
+	// 	<p>This is a paragraph.</p>
+	// </div>
+
+	// Display a h1 heading
+	// Display unordered list
+	// Display 3 list elements
+	<div>
+		<h1>List of Items</h1>
+		<ul>
+			<li>Item 1</li>
+			<li>Item 2</li>
+			<li>Item 3</li>
+		</ul>
+	</div>
+);
