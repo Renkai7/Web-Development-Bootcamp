24ec180ec7a80becbec291859b536310ccea50fb
Michael Spencer <ms53150@gmail.com>
2022-12-21 18:44:22 -0500

    create app file for components

---
+++ Section_33-React-js/react-basics/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Greeting from "./Greeting";
+
+import Heading from "./Heading";
+import List from "./List";
+
+function App() {
+	return (
+		<div>
+			<Heading />
+			<List />
+			<Greeting />
+		</div>
+	);
+}
+
+export default App;
