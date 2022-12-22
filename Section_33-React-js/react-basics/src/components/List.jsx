c94dea86879afcd81d5718eca177a619b5be5ccc
Michael Spencer <ms53150@gmail.com>
2022-12-22 18:47:53 -0500

    use math components to test imports

--- Section_33-React-js/react-basics/src/components/List.jsx
+++ Section_33-React-js/react-basics/src/components/List.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import pi, { doublePi, triplePi } from "./math";
+import { add, subtract, multiply, divide } from "./calculator";
 
 function List() {
 	return (
 		<ul>
-			<li>Item 1</li>
-			<li>Item 2</li>
-			<li>Item 3</li>
+			<li>{pi}</li>
+			<li>{doublePi()}</li>
+			<li>{triplePi()}</li>
+			<li>{add(1, 2)}</li>
+			<li>{multiply(2, 3)}</li>
+			<li>{subtract(7, 2)}</li>
+			<li>{divide(5, 2)}</li>
 		</ul>
 	);
 }
