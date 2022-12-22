eef8367944e3459b8efe90d93de2c1aa65363261
Michael Spencer <ms53150@gmail.com>
2022-12-22 18:46:12 -0500

    add contact component

---
+++ Section_33-React-js/react-basics/src/components/Contacts.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./Card";
+
+function Contacts() {
+	return (
+		<div>
+			<h1>My Contacts</h1>
+			<Card />
+		</div>
+	);
+}
+
+export default Contacts;
