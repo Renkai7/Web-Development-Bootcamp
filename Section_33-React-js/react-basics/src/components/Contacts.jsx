ce401961e001c1e22a1ffbb276167b94b739c147
Michael Spencer <ms53150@gmail.com>
2022-12-27 02:00:02 -0500

    create map to generate card lists

--- Section_33-React-js/react-basics/src/components/Contacts.jsx
+++ Section_33-React-js/react-basics/src/components/Contacts.jsx
@@ -1,11 +1,48 @@
 import React from "react";
 import Card from "./Card";
+import contacts from "./ContactList";
+
+function createCard(contact) {
+	return (
+		<Card
+			// id used to print contact.id to html (card component)
+			id={contact.id}
+			// key only for unique ID
+			key={contact.id}
+			name={contact.name}
+			img={contact.imgURL}
+			phone={contact.phone}
+			email={contact.email}
+		/>
+	);
+}
+
+// map() loops through an array and calls function for each item in array
+// map() replaces repeatedly calling Card components
 
 function Contacts() {
 	return (
 		<div>
-			<h1>My Contacts</h1>
-			<Card />
+			<h1 className="heading">My Contacts</h1>
+			{contacts.map(createCard)}
+			{/* <Card
+				name={contacts[0].name}
+				img={contacts[0].imgURL}
+				phone={contacts[0].phone}
+				email={contacts[0].email}
+			/>
+			<Card
+				name={contacts[1].name}
+				img={contacts[1].imgURL}
+				phone={contacts[1].phone}
+				email={contacts[1].email}
+			/>
+			<Card
+				name={contacts[2].name}
+				img={contacts[2].imgURL}
+				phone={contacts[2].phone}
+				email={contacts[2].email}
+			/> */}
 		</div>
 	);
 }
